6b14bc51663dc57299f8ae10d1c3b49d68ba0c1e
jonathan thompson <jonathan.thompson@hgs.hiddengeniusproject.com>
2024-06-17 10:15:27 -0400

    Initial Commit

---
+++ User AUTH/User_AUTHApp.swift
@@ -0,0 +1,17 @@
+//
+//  User_AUTHApp.swift
+//  User AUTH
+//
+//  Created by Jonathan Thompson on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct User_AUTHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
